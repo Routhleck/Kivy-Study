344b46eccc2094979c487a4253e232c48e6fcb98
Routhleck <1310722434@qq.com>
2022-06-26 22:30:41 +0800

    widget finish

--- project1_TheLab/main.py
+++ project1_TheLab/main.py
@@ -1,11 +1,53 @@
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty
+from kivy.properties import BooleanProperty
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
+from numpy import angle
+
+class ImagesExample(GridLayout):
+    speed = 0
+    def on_slider_value(self, widget):
+        self.speed = widget.value
+
+class WidgetsExample(GridLayout):
+    count = 1
+    count_enabled = BooleanProperty(False)
+    my_text = StringProperty("1")
+    text_input_str = StringProperty("foo")
+    #slider_value_txt = StringProperty("value")
+
+    def on_button_click(self):
+        print("Button clicked")
+        self.count += 1
+        self.my_text = str(self.count)
+        
+    def on_toggle_button_state(self, widget):
+        print("Toggle state: "+ widget.state)
+        if widget.state == "normal":
+            #OFF
+            widget.text = "OFF"
+            self.count_enabled = False
+        else:
+            #ON
+            widget.text = "ON"
+            self.count_enabled = True
+    
+    def on_switch_active(self, widget):
+        print("Switch active: "+ str(widget.active))
+        
+    def on_slider_value(self, widget):
+        print("Slider value: "+ str(int(widget.value)))
+        #self.slider_value_txt = str(int(widget.value))
+    
+    def on_text_validate(self, widget):
+        self.text_input_str = widget.text
+
 
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
